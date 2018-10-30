dd6bdb16c1114260d428b56a1c8bf97219cca946
kathiwa <katharina906@gmail.com>
2018-10-30 12:50:34 +0100

    Erstellung AnalyseScript.R

---
+++ AnalyseScript.R
@@ -0,0 +1,10 @@
+# Analyse Script
+print("Hier werden Pakete eingebunden")
+# Data Cleaning ----
+print("Hier wird der Datensatz aufbereitet. Thema am 09.11.2018")
+# Skalenberechnung ----
+print("Hier werden später Skalen berechnet. Thema am 09.11.2018")
+# Analyse ----
+print("Hier werden später statistische Analysen durchgeführt. Thema ab dem 16.11.2018")
+# Graphik erstellung ---- 
+print("Hier werden später Grafiken erstellt. Thema ab dem 16.11.2018")
