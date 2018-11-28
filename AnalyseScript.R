2225c2844c8bcf99e0882e0237ae36e167950f51
freya <freya@141-104.eduroam.rwth-aachen.de>
2018-11-28 11:11:46 +0100

    Deleted unnecessary columns

--- AnalyseScript.R
+++ AnalyseScript.R
@@ -1,7 +1,51 @@
 # Analyse Script
-print("Hier werden Pakete eingebunden")
+
+#### Bibliotheken laden
+install.packages("tidyverse")
+install.packages("lubridate")
+install.packages("psych")
+install.packages("esquisse")
+install.packages("ggthemes")
+install.packages("ggplot2")
+
+install.packages("devtools")
+library(devtools)
+devtools::install_github("HCIC/r-tools")
+
+source("surveymonkey.R")
+
+
 # Data Cleaning ----
-print("Hier wird der Datensatz aufbereitet. Thema am 09.11.2018")
+
+filename <- "data/DigitaleMuendigkeit.csv"
+raw <- load_surveymonkey_csv(filename)
+
+
+#### Daten cleanen----
+
+### Schritt 1: Unnötige Spalten löschen.
+raw.short <- raw[,c(-1:-9, -11:-14, -49:-98)] 
+
+### Schritt 2: Variablen umbenennen 
+## Variante 1:
+# names(raw.short)[1] <- "age" 
+# names(raw.short)[2] <- "gender"
+# names(raw.short)[3] <- "kut1"
+# usw...
+## diese Variante ist sehr umständlich.
+
+## Variante 2: Eine eigene Datei mit den Variablennamen erzeugen:
+generate_codebook(raw.short, "codebook.csv")
+# Dann codebook.csv in Excel öffnen, die Vairablennamen per Hand umbenennen, 
+# die Datei als codebook_final.csv abspeichern und hier wieder einlesen:
+codebook <- read_codebook("codebook_final.csv")
+
+#neue Namen auf die Daten anwenden:
+names(raw.short) <- codebook$variable
+
+
+
+
 # Skalenberechnung ----
 print("Hier werden später Skalen berechnet. Thema am 09.11.2018")
 # Analyse ----
